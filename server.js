0cca579ba05e6fd71e4198ab688a42148658eb9a
Pricila <pricilagenoveva@gmail.com>
2026-01-22 18:46:08 +0700

    connect mangodb

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import Product from './models/Product.js'; // import model
 // import routes lain jika ada
 // import productRoutes from './routes/productRoutes.js';
 // import authRoutes from './routes/authRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -66,9 +69,11 @@ app.delete('/api/products/:id', async (req, res) => {
 });
 
 // MongoDB connection
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
